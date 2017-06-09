3b3a692faaea1759318cc32c146797909308dcd6
Bas Hoonhout <bas.hoonhout@deltares.nl>
2017-06-09 10:25:26 +0200

    Added runall.py script

---
+++ xbeachtest/runall.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import paramiko
+
+HOSTNAME = 'h6'
+PORT = 22
+USERNAME = os.getenv('XBEACH_USER')
+PASSWORD = os.getenv'XBEACH_PASS')
+
+def runall():
+    s = paramiko.SSHClient()
+    s.load_system_host_keys()
+    s.connect(HOSTNAME, PORT, USERNAME, PASSWORD)
+    cmd = 'find /p/xbeach/skillbed/diagnostic/lastrun -name run.sh | awk \'{print "qsub \""$1"\""}\' | /bin/bash'
+    stdin, stdout, stderr = s.exec_command(cmd)
+    for line in stdout.readlines():
+        print line
+    s.close()
+
+    
+if __name__ == "main":
+    runall()
