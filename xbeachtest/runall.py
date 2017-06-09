8011ec87deff73f9489ac1ef14a46a721ee8194d
Bas Hoonhout <bas.hoonhout@deltares.nl>
2017-06-09 11:29:11 +0200

    Added teamcity messages

--- xbeachtest/runall.py
+++ xbeachtest/runall.py
@@ -7,15 +7,17 @@ HOSTNAME = 'h6'
 PORT = 22
 USERNAME = os.getenv('XBEACH_USER')
 PASSWORD = os.getenv('XBEACH_PASS')
+RUNDIR = os.getenv('XBEACH_DIAGNOSTIC_RUNLOCATION_UNIX')
 
 def runall():
     s = paramiko.SSHClient()
     s.load_system_host_keys()
     s.connect(HOSTNAME, PORT, USERNAME, PASSWORD)
-    cmd = 'find /p/xbeach/skillbed/diagnostic/lastrun -name run.sh | awk \'{print "qsub \""$1"\""}\' | /bin/bash'
+    cmd = 'find %s -name run.sh | awk \'{print "qsub \""$1"\""}\' | /bin/bash' % RUNDIR
+    print "##teamcity[message '%s']" % cmd
     stdin, stdout, stderr = s.exec_command(cmd)
     for line in stdout.readlines():
-        print line
+        print "##teamcity[message '%s']" % line
     s.close()
 
     
